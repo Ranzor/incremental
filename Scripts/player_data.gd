981de8aa6d872d8d74ece5c92453fd10d3dff6c1
Ørjan Sømme <ranzor1@gmail.com>
2024-11-07 13:06:23 +0100

    Fixed some menu bugs

--- Scripts/player_data.gd
+++ Scripts/player_data.gd
@@ -3,16 +3,16 @@ extends Node
 var upgrade_points : int = 0
 
 var fire_rate : float = 1.0
-var fire_rate_cost : int = 10
+var fire_rate_cost : int = 5
 
 var max_health : int = 20
-var max_health_cost : int = 10
+var max_health_cost : int = 15
 
 var damage : int = 10
-var damage_cost : int = 10
+var damage_cost : int = 20
 
 var multi_shot_level : int = 0
-var multi_shot_cost : int = 10
+var multi_shot_cost : int = 40
 
 func reset() -> void:
 	fire_rate = 1.0
--- Scripts/upgrade_menu.gd
+++ Scripts/upgrade_menu.gd
@@ -6,26 +6,52 @@ func _ready() -> void:
 	$Panel/VBoxContainer/DamageButton.connect("pressed", Callable(self,"on_damage_upgrade"))
 	$Panel/VBoxContainer/MultiShotButton.connect("pressed", Callable(self,"on_multi_shot_upgrade"))
 	$Panel/VBoxContainer/RestartButton.connect("pressed", Callable(self,"restart"))
+	set_buttons()
 	
 func on_fire_rate_upgrade() -> void:
 	if PlayerData.upgrade_points >= PlayerData.fire_rate_cost:
 		PlayerData.upgrade_points -= PlayerData.fire_rate_cost
 		PlayerData.fire_rate = max(PlayerData.fire_rate -0.1, 0.2)
+		set_buttons()
 		
 func on_health_upgrade() -> void:
-	if PlayerData.upgrade_points >= PlayerData.max_health_cost:
+	if PlayerData.upgrade_points >= PlayerData.max_health_cost:		
 		PlayerData.upgrade_points -= PlayerData.max_health_cost
-		PlayerData.max_health += 20
+		PlayerData.max_health += 20	
+		set_buttons()
 		
 func on_damage_upgrade() -> void:
-	if PlayerData.upgrade_points >= PlayerData.damage_cost:
+	if PlayerData.upgrade_points >= PlayerData.damage_cost:		
 		PlayerData.upgrade_points -= PlayerData.damage_cost
 		PlayerData.damage += 10
+		set_buttons()
 
 func on_multi_shot_upgrade() -> void:
 	if PlayerData.upgrade_points >= PlayerData.multi_shot_cost:
 		PlayerData.upgrade_points -= PlayerData.multi_shot_cost
 		PlayerData.multi_shot_level += 1
+		set_buttons()
 		
 func restart() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	
+func set_buttons() -> void:
+	if PlayerData.upgrade_points >= PlayerData.fire_rate_cost:
+		$Panel/VBoxContainer/FireRateButton.disabled = false
+	else:
+		$Panel/VBoxContainer/FireRateButton.disabled = true
+		
+	if PlayerData.upgrade_points >= PlayerData.max_health_cost:
+		$Panel/VBoxContainer/HealthButton.disabled = false
+	else:
+		$Panel/VBoxContainer/HealthButton.disabled = true
+		
+	if PlayerData.upgrade_points >= PlayerData.damage_cost:
+		$Panel/VBoxContainer/DamageButton.disabled = false	
+	else:
+		$Panel/VBoxContainer/DamageButton.disabled = true
+	
+	if PlayerData.upgrade_points >= PlayerData.multi_shot_cost:
+		$Panel/VBoxContainer/MultiShotButton.disabled = false
+	else:
+		$Panel/VBoxContainer/MultiShotButton.disabled = true
